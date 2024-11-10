1b86f5c0130680174994d46bf98c885dd78f9c36
Cameron Ingham <camji555@gmail.com>
2024-11-10 01:29:56 -0800

    Remove HealthKit Dependency & Upgrade to Swift 6

--- Sources/LoopAlgorithm/LoopUnit.swift
+++ Sources/LoopAlgorithm/LoopUnit.swift
@@ -13,6 +13,9 @@ public enum LoopUnit: Sendable, CaseIterable {
     case milligramsPerDeciliter
     case milligramsPerDeciliterPerSecond
     case milligramsPerDeciliterPerMinute
+    case millimolesPerLiter
+    case millimolesPerLiterPerSecond
+    case millimolesPerLiterPerMinute
     case percent
     
     public init(from string: String) {
@@ -26,17 +29,41 @@ public enum LoopUnit: Sendable, CaseIterable {
              (.milligramsPerDeciliter, .milligramsPerDeciliter),
              (.milligramsPerDeciliterPerSecond, .milligramsPerDeciliterPerSecond),
              (.milligramsPerDeciliterPerMinute, .milligramsPerDeciliterPerMinute),
+             (.millimolesPerLiter, .millimolesPerLiter),
+             (.millimolesPerLiterPerSecond, .millimolesPerLiterPerSecond),
+             (.millimolesPerLiterPerMinute, .millimolesPerLiterPerMinute),
              (.percent, .percent):
             return 1
-        case (.milligramsPerDeciliterPerSecond, .milligramsPerDeciliterPerMinute):
+        case (.milligramsPerDeciliterPerSecond, .milligramsPerDeciliterPerMinute),
+             (.millimolesPerLiterPerSecond, .millimolesPerLiterPerMinute):
             return 60
-        case (.milligramsPerDeciliterPerMinute, .milligramsPerDeciliterPerSecond):
+        case (.milligramsPerDeciliterPerMinute, .milligramsPerDeciliterPerSecond),
+             (.millimolesPerLiterPerMinute, .millimolesPerLiterPerSecond):
             return 1/60
+        case (.milligramsPerDeciliterPerSecond, .millimolesPerLiterPerSecond):
+            return 0.0555
+        case (.milligramsPerDeciliterPerSecond, .millimolesPerLiterPerMinute):
+            return 0.0555 * 60
+        case (.milligramsPerDeciliterPerMinute, .millimolesPerLiterPerSecond):
+            return 0.0555 / 60
+        case (.milligramsPerDeciliterPerMinute, .millimolesPerLiterPerMinute):
+            return 0.0555
+        case (.millimolesPerLiterPerSecond, .milligramsPerDeciliterPerSecond):
+            return 18.018
+        case (.millimolesPerLiterPerSecond, .milligramsPerDeciliterPerMinute):
+            return 18.018 * 60
+        case (.millimolesPerLiterPerMinute, .milligramsPerDeciliterPerSecond):
+            return 18.018 / 60
+        case (.millimolesPerLiterPerMinute, .milligramsPerDeciliterPerMinute):
+            return 18.018
         case (.gram, _),
              (.internationalUnit, _),
              (.milligramsPerDeciliter, _),
              (.milligramsPerDeciliterPerSecond, _),
              (.milligramsPerDeciliterPerMinute, _),
+             (.millimolesPerLiter, _),
+             (.millimolesPerLiterPerSecond, _),
+             (.millimolesPerLiterPerMinute, _),
              (.percent, _):
             return nil
         }
@@ -54,6 +81,12 @@ public enum LoopUnit: Sendable, CaseIterable {
             return "mg/dL·s"
         case .milligramsPerDeciliterPerMinute:
             return "mg/min·dL"
+        case .millimolesPerLiter:
+            return "mmol/L"
+        case .millimolesPerLiterPerSecond:
+            return "mmol/L·s"
+        case .millimolesPerLiterPerMinute:
+            return "mmol/min·L"
         case .internationalUnit:
             return "IU"
         }

de43e8e34a39f02fbe552447ed10a4259cfafcda
Cameron Ingham <camji555@gmail.com>
2024-11-12 14:02:41 -0800

    Remove HealthKit Dependency & Upgrade to Swift 6

--- Sources/LoopAlgorithm/LoopQuantity.swift
+++ Sources/LoopAlgorithm/LoopQuantity.swift
@@ -60,9 +60,9 @@ public struct LoopQuantity: Hashable, Equatable, Comparable, Sendable {
     
     public static func == (lhs: LoopQuantity, rhs: LoopQuantity) -> Bool {
         guard rhs.is(compatibleWith: lhs.unit) else {
-            return lhs == rhs
+            return lhs.unit == rhs.unit && lhs.value == rhs.value
         }
         
-        return lhs == rhs && (lhs.doubleValue(for: lhs.unit) == rhs.doubleValue(for: lhs.unit))
+        return lhs.unit == rhs.unit && lhs.value == rhs.value && (lhs.doubleValue(for: lhs.unit) == rhs.doubleValue(for: lhs.unit))
     }
 }
